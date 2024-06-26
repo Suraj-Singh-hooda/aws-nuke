37c62eaa1aa3346c2b35fb95e22947e06a38ae80
bfisher8 <bradley.fisher@optum.com>
2024-06-26 09:42:33 -0500

    Add option to ingore KMS errors due to permissions

--- cmd/nuke.go
+++ cmd/nuke.go
@@ -272,7 +272,7 @@ func (n *Nuke) HandleQueue() {
 func (n *Nuke) HandleRemove(item *Item) {
 	err := item.Resource.Remove()
 	if err != nil && err.Error() == "ignoring error" {
-		item.State = ItemStateIgnored
+		item.State = ItemStateFinished
 		item.Reason = ""
 		return
 	} else if err != nil {
